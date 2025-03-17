6fa8700183c6d3e8f70ed055ca8aa0d78a5468d5
lennertamas <tamas.lenner@gmail.com>
2025-03-17 13:24:35 +0100

    update

--- SQL/exercisse_3_order_of_customer.sql
+++ SQL/exercisse_3_order_of_customer.sql
@@ -2,5 +2,5 @@
 The attached database contains data of order handling system of Sunshine Happiness Pizzeria. Pizza table contains details of orderable pizzas, Customer table contains basic data of customers and Couirier contains name of couriers. 
 All of them are connected through the Orders table that contains every order information.
 
-Write a query that lists how many pizzas each courier delivered in descending order based on the quantity delivered.
+Write a query that lists how many pizzas are delivered by every each courier. Select the records in descending order by the quantity delivered.
 */
---
+++ SQL/exercisse_4_order_of_customers.sql
@@ -0,0 +1,7 @@
+/*
+The attached database contains data of order handling system of Sunshine Happiness Pizzeria. Pizza table contains details of orderable pizzas, Customer table contains basic data of customers and Couirier contains name of couriers. 
+All of them are connected through the Orders table that contains every order information.
+
+Write a query that lists the customers by the amount of money they paid for pizzas in descending order by amount. Use window function to give 
+rank for customers. The customer who buys the most gets the 1. rank. In case of same rank the numbering should be continuous, without missing any numbers. 
+*/
